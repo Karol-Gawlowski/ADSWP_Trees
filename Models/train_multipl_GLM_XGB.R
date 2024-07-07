f91e869a64d4badd8fa1d8963613deb410a47d7d
Karol Gawlowski <karol.gawlowski@cass.city.ac.uk>
2024-07-07 12:17:24 +0100

    multiplicative ens

---
+++ Models/train_multipl_GLM_XGB.R
@@ -0,0 +1,41 @@
+train_multipl_GLM_XGB = function(glm_model,
+                                 dt,
+                                 y,
+                                 vdt){
+  
+  glm_preds_in = unname(predict(glm_model,dt,type="response"))
+  glm_preds_out = unname(predict(glm_model,vdt$x_val,type="response"))
+  
+  xgb_model = train_XGBoost(dt = dt,
+                            y = y/glm_preds_in,
+                            vdt = list(x_val = vdt$x_val,
+                                       y_val = vdt$y_val/glm_preds_out),
+                            
+                            objective = "count:poisson",
+                            eval_metric = "poisson-nloglik",
+                            eta = 0.005,
+                            max_depth = 5,
+                            tweedie_variance_power = NULL
+  )
+  
+  toreturn = list(glm_model = glm_model,xgb_model = xgb_model)
+  
+  class(toreturn) = "train_multipl_GLM_XGB" 
+  
+  return(toreturn)
+  
+}
+
+predict.train_multipl_GLM_XGB = function(model,dt){
+
+  glm = unname(predict(model$glm_model,dt,type="response"))
+  xgb = predict(model$xgb_model,xgb.DMatrix(data.matrix(dt)),type="response")
+
+  toreturn = glm * xgb
+  
+  if(any(is.na(toreturn) | is.nan(toreturn) | toreturn<0)){browser()}
+    
+  return(toreturn)
+  
+}
+
--- the_actuary_Trees.R
+++ the_actuary_Trees.R
@@ -4,14 +4,19 @@ source("Models/train_GLM_w_XGB.R")
 
 CV = 5
 
+set.seed(1)
+
 CV_vec = sample(1:CV,replace = T,size = nrow(dt_list$fre_mtpl2_freq))
 
 models = list()
 results = list()
 losses = data.frame(CV = paste0("CV_",1:CV),
+                    homog = NA,
                     glm = NA,
                     XGB = NA,
-                    GLM_XGB = NA)
+                    train_GLM_w_XGB = NA,
+                    GLM_XGB = NA,
+                    multipl_GLM_XGB = NA)
 
 # fitted = readRDS("The_Actuary_final_results_wo_models_v5.rds")
 # 
@@ -29,7 +34,8 @@ for (i in 1:CV){
                                           glm = NA,
                                           XGB = NA,
                                           train_GLM_w_XGB = NA,
-                                          GLM_XGB = NA) %>% 
+                                          GLM_XGB = NA,
+                                          multipl_GLM_XGB = NA) %>% 
     mutate(homog = mean(dt_list$fre_mtpl2_freq$ClaimNb[train_rows]))
   
   encoder = preproc(dt_frame = dt_list$fre_mtpl2_freq[train_rows,],
@@ -109,14 +115,30 @@ for (i in 1:CV){
   )
   
   results[[paste0("CV_",i)]]$GLM_XGB = pmax(0,predict(models[[paste0("CV_",i)]]$GLM_XGB_model,
-                                               xgb.DMatrix(data.matrix(dt_list$fre_mtpl2_freq[-train_rows,-c(1,2,3)]))) +
-    results[[paste0("CV_",i)]]$glm)
+                                                      xgb.DMatrix(data.matrix(dt_list$fre_mtpl2_freq[-train_rows,-c(1,2,3)]))) +
+                                              results[[paste0("CV_",i)]]$glm)
   
   losses$GLM_XGB[i] = poisson_deviance(y_true = results[[paste0("CV_",i)]]$actual,
                                        y_pred = results[[paste0("CV_",i)]]$GLM_XGB)
   
   
+  # GLM * XGBoost  -------------------------------------------
+  
+  models[[paste0("CV_",i)]]$multipl_GLM_XGB = train_multipl_GLM_XGB(glm_model = models[[paste0("CV_",i)]]$glm_model,
+                                                                    dt = dt_list$fre_mtpl2_freq[train_rows,-c(1,2,3)],
+                                                                    y = dt_list$fre_mtpl2_freq$ClaimNb[train_rows],
+                                                                    vdt = list(x_val = dt_list$fre_mtpl2_freq[-train_rows,-c(1,2,3)],
+                                                                               y_val = dt_list$fre_mtpl2_freq$ClaimNb[-train_rows])
+  )
+  
+  results[[paste0("CV_",i)]]$multipl_GLM_XGB = predict(models[[paste0("CV_",i)]]$multipl_GLM_XGB,
+                                                       dt_list$fre_mtpl2_freq[-train_rows,-c(1,2,3)])
+  
+  losses$multipl_GLM_XGB[i] = poisson_deviance(y_true = results[[paste0("CV_",i)]]$actual,
+                                               y_pred = results[[paste0("CV_",i)]]$multipl_GLM_XGB)
+  
 }
+
 
 # save files
 # saveRDS(list(losses = losses,
@@ -124,25 +146,47 @@ for (i in 1:CV){
 #              models = models),file = "The_Actuary_trees_v1.rds")
 
 saveRDS(list(losses = losses,
-             results = results),file = "Results/The_Actuary_trees_wo_models_v2.rds")
+             results = results),file = "Results/The_Actuary_trees_wo_models_v5.rds")
+
+# check calibration
+bind_rows(results,.id = "id") %>% 
+  select(-ID) %>% 
+  group_by(id) %>% 
+  summarise_all(mean)
+
+# avg deviation from actual %
+bind_rows(results,.id = "id") %>% 
+  select(-ID) %>% 
+  group_by(id) %>% 
+  summarise_all(mean) %>% 
+  mutate_if(is.numeric,~if_else(.==actual,.,./actual - 1)) %>% 
+  select(-actual) %>% 
+  mutate_if(is.numeric,scales::percent,0.1)
 
 analysis = bind_rows(results,.id = "id")  %>% 
-  select(id,actual,glm,XGB, homog, train_GLM_w_XGB, GLM_XGB) %>% 
-  pivot_longer(cols = glm:GLM_XGB) %>% 
+  select(id,actual,glm,XGB, homog, train_GLM_w_XGB, GLM_XGB,multipl_GLM_XGB) %>% 
+  pivot_longer(cols = glm:multipl_GLM_XGB) %>% 
   mutate(actual = actual,
          value = value,
          poiss = Vectorize(poisson_deviance)(y_true = actual,
                                              y_pred = value)) 
 
 # ovarall and per fold results
-rbind(losses,
-      losses %>%
-        pivot_longer(cols = !CV) %>%
-        group_by(name) %>%
-        summarise(mean_poiss = mean(value)) %>%
-        arrange(mean_poiss) %>%
-        pivot_wider(values_from = mean_poiss,names_from = name) %>%
-        mutate(CV = "mean_poiss"))
+poiss_per_CV = rbind(losses,
+                     losses %>%
+                       pivot_longer(cols = !CV) %>%
+                       group_by(name) %>%
+                       summarise(mean_poiss = mean(value)) %>%
+                       arrange(mean_poiss) %>%
+                       pivot_wider(values_from = mean_poiss,names_from = name) %>%
+                       mutate(CV = "mean_poiss"))
+
+# pinball
+poiss_per_CV %>% 
+  mutate_if(is.numeric,~ if_else(. == homog, .,1 -  ./homog)) %>% 
+  select(-homog) %>% 
+  mutate_if(is.numeric,scales::percent,0.1)
+  
 
 losses %>% 
   mutate_if(is.numeric,.funs = function(x)(x*c(data.frame(k=CV_vec) %>% 
@@ -150,11 +194,12 @@ losses %>%
   janitor::adorn_totals()
 
 analysis %>%
-  filter(name!="homog") %>% 
+  filter(name!="homog") %>%
+  # filter(name %in% c("train_GLM_w_XGB","multipl_GLM_XGB")) %>% 
   rename(model=name) %>% 
   ggplot(aes(x = poiss,fill=model,color=model,linetype=model))+
   geom_density(alpha=0.3,size=1)+
-  ggplot2::scale_fill_manual(values = c("blue","yellow","green","red"))+
+  ggplot2::scale_fill_manual(values = c("blue","yellow","green","red","white"))+
   xlim(0,0.75)+
   # facet_wrap(~name)+
   ggdark::dark_theme_classic()+
@@ -172,3 +217,9 @@ multiple_lift(y_true = bind_rows(results,.id = "id") %>% pull(actual),
   xlab("Tiles")+
   ylab("Implied frequency")+
   ggdark::dark_theme_classic()
+
+# remark on glm
+merge(coefficients(models$CV_1$train_GLM_w_XGB$glm_model) %>% data.frame() %>% set_names("glm(xgb)") %>% rownames_to_column() ,
+      coefficients(models$CV_1$glm_model) %>% data.frame()  %>% set_names("glm") %>% rownames_to_column(),
+      by = "rowname",all.x=T) %>% mutate(diff_perc = scales::percent(`glm(xgb)`/glm - 1,0.1)) %>% mutate_at(vars(contains("glm")),scales::number,0.01)
+
